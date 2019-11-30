ef26de5a0b3332a7c83080b902aa39610a49a99f
MuszkaCPP <adam.muszka@op.pl>
2019-11-30 11:52:08 +0100

    +ChatWindow, +Button

--- src/main/java/go/mik/UI/UIWindow.java
+++ src/main/java/go/mik/UI/UIWindow.java
@@ -1,27 +1,32 @@
-package go.UI;
+package go.mik.UI;
 
-import go.UI.Components.UI_GameField;
+import go.mik.UI.Components.UI_GameField;
+import go.mik.UI.Controllers.ButtonController;
 
-import java.awt.Color;
-import java.awt.Graphics;
+import java.awt.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 
-import javax.swing.Action;
-import javax.swing.JFrame;
-import javax.swing.JOptionPane;
+import javax.swing.*;
+import javax.swing.border.EmptyBorder;
 
 
 public class UIWindow extends JFrame implements ActionListener{
 	
 	private int _pickedSocketID;
 	private static UI_GameField _gameField;
+	private ButtonController _buttonController;
+	private JButton _blackStoneBtn;
+	private JPanel _mainPanel = new JPanel(new BorderLayout(2,2));
+	private JPanel _chatAndButtonsPanel = new JPanel(new GridLayout(2,1));
+	private JScrollPane _chat = new JScrollPane();
 	
-	UIWindow(){
+	public UIWindow(){
 		super();
 		takeInputForWindow();
 		createWindow();
-		createGameField(100,100);
+		createGameField(19,19);
+		initializeButtons();
 	}
 	
 	private void takeInputForWindow() {
@@ -37,12 +42,12 @@ public class UIWindow extends JFrame implements ActionListener{
 	}
 	
 	private void createWindow() {
-		//setResizable(false);
+		setResizable(false);
 		setTitle("Go game");
-		setSize(1000, 1000);
+		setSize(1500, 1000);
 		setDefaultCloseOperation(EXIT_ON_CLOSE);
+		setContentPane(_mainPanel);
 		setVisible(true);
-		
 	}
 	
 	private void createGameField(int verFieldAmount, int horFieldAmount) {
@@ -52,16 +57,36 @@ public class UIWindow extends JFrame implements ActionListener{
 				.windowSizeX(getWidth())
 				.windowSizeY(getHeight())
 				.buildGameField();
-		
-		add(_gameField);
-		_gameField.setVisible(true);
+
 		_gameField.repaint();
+		_gameField.setBorder(new EmptyBorder(0,0,0,50));
+		_gameField.setBackground(Color.WHITE);
+
+		_mainPanel.add(_gameField, BorderLayout.CENTER);
+
+	}
+
+	private void initializeButtons(){
+		_buttonController = new ButtonController(this);
+		_blackStoneBtn = new JButton("PASS");
+		_blackStoneBtn.setBackground(Color.white);
+		_blackStoneBtn.setBorderPainted(false);
+
+		_chat.setBorder(new EmptyBorder(0,0,0,500));
+
+		_chatAndButtonsPanel.setBorder(new EmptyBorder(50,0,20,50));
+		_chatAndButtonsPanel.add(_chat);
+		_chatAndButtonsPanel.add(_blackStoneBtn);
+		_chat.setBackground(Color.BLACK);
+
+		_mainPanel.add(_chatAndButtonsPanel, BorderLayout.EAST);
+
 	}
 
 	@Override
 	public void actionPerformed(ActionEvent action) {
-		if(action.getSource() == null) {
-			
+		if(action.getSource() == this._blackStoneBtn) {
+			_buttonController.doCertainAction("Pick field");
 		}
 		
 	}
--- src/main/java/go/mik/UI/UI_Launcher.java
+++ src/main/java/go/mik/UI/UI_Launcher.java
@@ -1,5 +1,7 @@
 package go.mik.UI;
 
+import go.mik.UI.UIWindow;
+
 public class UI_Launcher {
 
 	public static void main(String[] args) {

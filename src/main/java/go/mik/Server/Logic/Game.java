fe40d50d8d511fb1d490c0cab091720e76ab6201
Eryk <37209877+PanOrka@users.noreply.github.com>
2019-12-03 12:26:13 +0100

    + game

--- src/main/java/go/mik/Server/Logic/Game.java
+++ src/main/java/go/mik/Server/Logic/Game.java
@@ -1,9 +1,35 @@
 package go.mik.Server.Logic;
 
 abstract class Game {
-    synchronized String move(String command) {
-        // NO I TUTAJ ZABAWA W ZASADY GRY oraz sama gre ofc !!!
+    private final char[][] field;
+
+    Game() {
+        this.field = new char[19][19];
+        for (int i=0; i<19; i++) {
+            for (char chars : this.field[i]) {
+                chars = 'o';
+            }
+        }
+    }
+
+    synchronized String move(String command, char color) {
+        String[] str = command.split(";");
+        int x = this.parser(str[0]);
+        int y = this.parser(str[1]);
+
+        if (this.field[x][y] == 'b' || this.field[x][y] == 'w') {
+            return "CHAT:This is occupied";
+        }
 
         return "nothing now";
+    }
+
+    private int parser(String number) {
+        try {
+            return Integer.parseInt(number);
+        } catch(NumberFormatException ex) {
+            System.err.println(ex.getMessage());
+            return 0;
+        }
     }
 }

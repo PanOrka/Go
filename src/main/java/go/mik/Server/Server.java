64eb49b01c5ae4f12cc5b3efd8b2ef8ed84384c9
Eryk <37209877+PanOrka@users.noreply.github.com>
2019-12-11 01:51:52 +0100

    + More players / bot prep

--- src/main/java/go/mik/Server/Server.java
+++ src/main/java/go/mik/Server/Server.java
@@ -1,15 +1,18 @@
 package go.mik.Server;
 
+import go.mik.Server.Logic.Game.GameSystem;
 import go.mik.Server.Logic.Game.GameSystemInterface;
 import go.mik.Server.Logic.PlayerConnector.Player;
 import go.mik.Server.Logic.PlayerConnector.PlayerConnector;
 
 import java.io.IOException;
 import java.net.ServerSocket;
+import java.net.Socket;
+import java.util.Scanner;
 import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
 
-public class Server extends GameSystemInterface {
+public class Server {
     private int socketPort;
 
     public Server(int socketPort) {
@@ -20,13 +23,31 @@ public class Server extends GameSystemInterface {
     void start() {
         try (ServerSocket listener = new ServerSocket(socketPort)) {
             System.out.println("Server is Running");
-            ExecutorService pool = Executors.newFixedThreadPool(2);
+            ExecutorService pool = Executors.newFixedThreadPool(200);
 
-            PlayerConnector playerConnector1 = new Player(listener.accept(), 'b', this);
-            PlayerConnector playerConnector2 = new Player(listener.accept(), 'w', this);
+            while (true) {
 
-            pool.execute(playerConnector1);
-            pool.execute(playerConnector2);
+                Socket connectorSocket = listener.accept();
+                Scanner gameMakerInput = new Scanner(connectorSocket.getInputStream());
+
+                if (gameMakerInput.nextLine().startsWith("PLAY:BOT")) {
+                    GameSystemInterface gameSystem = new GameSystem();
+
+                    PlayerConnector playerConnector1 = new Player(connectorSocket, 'b', gameSystem);
+                    pool.execute(playerConnector1);
+
+                    PlayerConnector playerConnector2 = new Player(connectorSocket, 'w', gameSystem);
+                    pool.execute(playerConnector2);
+                } else if (gameMakerInput.nextLine().startsWith("PLAY:PVP")) {
+                    GameSystemInterface gameSystem = new GameSystem();
+
+                    PlayerConnector playerConnector1 = new Player(connectorSocket, 'b', gameSystem);
+                    pool.execute(playerConnector1);
+
+                    PlayerConnector playerConnector2 = new Player(connectorSocket, 'w', gameSystem);
+                    pool.execute(playerConnector2);
+                }
+            }
         } catch(IOException ioException) {
             System.err.println(ioException.getMessage());
         }

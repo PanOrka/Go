0c7f389bc905b7f71344fbc4dd16fd5a4d221268
Eryk <37209877+PanOrka@users.noreply.github.com>
2019-12-18 22:16:12 +0100

    + bot FINAL BOTTO POGROMCA

--- src/main/java/go/mik/Bot/Logics/NormalBotLogic.java
+++ src/main/java/go/mik/Bot/Logics/NormalBotLogic.java
@@ -2,26 +2,55 @@ package go.mik.Bot.Logics;
 
 import go.mik.Bot.Bots.Bot;
 
+import java.util.Random;
+
 public class NormalBotLogic extends BotLogic {
     private final int killWeight = 1;
-    private final int preventKillWeight = 1;
+    private final int killWeightIfOne = 80;
+    private final int preventKillWeight = 70;
+    private final int fieldEmptyWeight = 2; // if same weight closer then nothing
+    private final int fieldSameWeight = 4;
+    private final int fieldDiffFurtherWeight = -1;
+    private final int fieldDiffCloserWeight = -2;
+    private final int extraStrategicalWeight = 3; // jak bylo 2 to nie zaczynal kolo sciany
+    private final int extraWallWeight = 10;
 
     private int killPoints; // PUNKTY ZA KILL
+    private int killPreventPoints; // PUNKTY ZA PREVENT
+    private int fieldTakePoints; // PUNKTY ZA OCZKA
+    private int extraStrategicalPoints;
     private boolean[] isEmptyKill;
     private boolean isEmptySuicide;
     private int killCounter, killTempPoints;
-    private int killPreventPoints; // PUNKTY ZA PREVENT
+
+    private final Random random;
 
     public NormalBotLogic(Bot bot) {
         super(bot);
         this.isEmptyKill = new boolean[4];
+        this.random = new Random();
     }
 
     @Override
     public void makeTurn() {
+        this.setWeights();
         for (int i=0; i<19; i++) {
             for (int k=0; k<19; k++) {
                 this.setPoints();
+                if ((i == 1) || (i == 17)) {
+                    this.extraStrategicalPoints += this.extraStrategicalWeight;
+                }
+                if ((k == 1) || (k == 17)) {
+                    this.extraStrategicalPoints += this.extraStrategicalWeight;
+                }
+
+                if ((i == 0) || (i == 18)) {
+                    this.extraStrategicalPoints += this.extraWallWeight;
+                }
+                if ((k == 0) || (k == 18)) {
+                    this.extraStrategicalPoints += this.extraWallWeight;
+                }
+
                 if (this.field[i][k].isOccupied != 'o') {
                     this.field[i][k].weight = -1;
                     continue;
@@ -45,18 +74,24 @@ public class NormalBotLogic extends BotLogic {
                         this.field[i][k].weight = -1;
                         continue;
                     }
+                } else if (this.killPoints == 1) {
+                    this.killPoints += killWeightIfOne;
                 }
 
                 this.field[i][k].isOccupied = this.opponentColor;
                 this.calculatePreventKill(i, k);
                 this.field[i][k].isOccupied = 'o';
 
+                this.field[i][k].isOccupied = this.actualColor;
+                this.calculateFieldTake(i, k);
+                this.field[i][k].isOccupied = 'o';
+
                 this.addWeights(i, k);
             }
         }
 
-        int bestX = 10;
-        int bestY = 10;
+        int bestX = random.nextInt(19);
+        int bestY = random.nextInt(19);
 
         for (int i=0; i<19; i++) {
             for (int k=0; k<19; k++) {
@@ -72,6 +107,75 @@ public class NormalBotLogic extends BotLogic {
         System.out.println("MOJA WAGA: " + this.field[bestX][bestY].weight);
         this.botMove(bestX + ";" + bestY);
     }
+
+
+
+    ////////////////////////////////////// FIELD TAKE //////////////////////////////////////////////////////////////////
+
+    private void calculateFieldTake(int x, int y) {
+        //EMPTY
+        if (x+1 < 19 && this.field[x+1][y].isOccupied == 'o') {
+            checkFields(x+1, y);
+        } else if (x+1 < 19 && this.field[x+1][y].isOccupied == this.opponentColor) {
+            this.fieldTakePoints += this.fieldDiffCloserWeight;
+        }
+
+        if (x-1 >= 0 && this.field[x-1][y].isOccupied == 'o') {
+            checkFields(x-1, y);
+        } else if (x-1 >= 0 && this.field[x-1][y].isOccupied == this.opponentColor) {
+            this.fieldTakePoints += this.fieldDiffCloserWeight;
+        }
+
+        if (y+1 < 19 && this.field[x][y+1].isOccupied == 'o') {
+            checkFields(x, y+1);
+        } else if (y+1 < 19 && this.field[x][y+1].isOccupied == this.opponentColor) {
+            this.fieldTakePoints += this.fieldDiffCloserWeight;
+        }
+
+        if (y-1 >= 0 && this.field[x][y-1].isOccupied == 'o') {
+            checkFields(x, y-1);
+        } else if (y-1 >= 0 && this.field[x][y-1].isOccupied == this.opponentColor) {
+            this.fieldTakePoints += this.fieldDiffCloserWeight;
+        }
+
+    }
+
+    private void checkFields(int x, int y) {
+        if (x+1 < 19 && this.field[x+1][y].isOccupied == 'o') {
+            this.fieldTakePoints += this.fieldEmptyWeight;
+        } else if (x+1 < 19 && this.field[x+1][y].isOccupied == this.actualColor) {
+            this.fieldTakePoints += this.fieldSameWeight;
+        } else if (x+1 < 19 && this.field[x+1][y].isOccupied == this.opponentColor) {
+            this.fieldTakePoints += this.fieldDiffFurtherWeight;
+        }
+
+        if (x-1 >= 0 && this.field[x-1][y].isOccupied == 'o') {
+            this.fieldTakePoints += this.fieldEmptyWeight;
+        } else if (x-1 >= 0 && this.field[x-1][y].isOccupied == this.actualColor) {
+            this.fieldTakePoints += this.fieldSameWeight;
+        } else if (x-1 >= 0 && this.field[x-1][y].isOccupied == this.opponentColor) {
+            this.fieldTakePoints += this.fieldDiffFurtherWeight;
+        }
+
+        if (y+1 < 19 && this.field[x][y+1].isOccupied == 'o') {
+            this.fieldTakePoints += this.fieldEmptyWeight;
+        } else if (y+1 < 19 && this.field[x][y+1].isOccupied == this.actualColor) {
+            this.fieldTakePoints += this.fieldSameWeight;
+        } else if (y+1 < 19 && this.field[x][y+1].isOccupied == this.opponentColor) {
+            this.fieldTakePoints += this.fieldDiffFurtherWeight;
+        }
+
+        if (y-1 >= 0 && this.field[x][y-1].isOccupied == 'o') {
+            this.fieldTakePoints += this.fieldEmptyWeight;
+        } else if (y-1 >= 0 && this.field[x][y-1].isOccupied == this.actualColor) {
+            this.fieldTakePoints += this.fieldSameWeight;
+        } else if (y-1 >= 0 && this.field[x][y-1].isOccupied == this.opponentColor) {
+            this.fieldTakePoints += this.fieldDiffFurtherWeight;
+        }
+
+    }
+
+    //////////////////////////////////// END FIELD TAKE ////////////////////////////////////////////////////////////////
 
 
 
@@ -311,10 +415,22 @@ public class NormalBotLogic extends BotLogic {
     private void setPoints() {
         this.killPoints = 0;
         this.killPreventPoints = 0;
+        this.fieldTakePoints = 0;
+        this.extraStrategicalPoints = 0;
+    }
+
+    private void setWeights() {
+        for (int i=0; i<19; i++) {
+            for (int k=0; k<19; k++) {
+                this.field[i][k].weight = 0;
+            }
+        }
     }
 
     private void addWeights(int x, int y) {
         this.field[x][y].weight += this.killPoints;
         this.field[x][y].weight += this.killPreventPoints;
+        this.field[x][y].weight += this.fieldTakePoints;
+        this.field[x][y].weight += this.extraStrategicalPoints;
     }
 }
